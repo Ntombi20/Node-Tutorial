afd756d202f18175a2c9b91c26549e82835b0fd5
Ntombi20 <ntombi@projectcodex.co>
2016-03-30 16:05:04 +0200

    done with filter number

--- node-tutorial/users.js
+++ node-tutorial/users.js
@@ -5,8 +5,14 @@ var users = [];
 //using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
 
 //Create 3 users : Jack, Callum and Neil - they all should have the name Bradshaw
+users.push({ first_name : 'Jack', last_name : 'Bradshaw' });
+users.push({ first_name : 'Callum', last_name : 'Bradshaw' });
+users.push({ first_name : 'Neil', last_name : 'Bradshaw' });
 
 //now loop through the users array and write the following data to the console below each other for each user
+for (var i = 0; i < users.length; i++) {
+  users[i]
+}
 //you can use the forEach method
 
 //Hi, first_name last_name!
