1d38dd98aabed3b3bf86bdc8499a10b7505bbbf1
Ntombi20 <ntombi@projectcodex.co>
2016-04-15 15:16:48 +0200

    got the total items sold

--- node-tutorial/products.js
+++ node-tutorial/products.js
@@ -1,9 +1,7 @@
 module.exports = function(filePath){
 
-//returns a list of products
-
 var fs = require('fs');
-
+//returns a list of products..
 this.productNames = function(callback){
     var productList = [];
     var readFile = fs.readFileSync(filePath, "utf8");
@@ -24,15 +22,29 @@ this.productNames = function(callback){
         callback(null, productList);
 }
 
+//returns a map of how many of each product is sold - mapping productName to quantity sold.
+this.productsSold = function(callback){
+  var productMap = {};
+  var readFile = fs.readFileSync(filePath, "utf8");
 
-//return products;
+      var items = readFile.split("\n");
 
-// }
-//
-// this.productsSold = function() {
-//
-//
-//
-// }
-//
+      items.forEach(function(item) {
+
+      var hold = item.split(',');
+
+      var currentItem = hold[1];
+      var NumberOfItem = hold[2];
+
+        if(productMap[currentItem] === undefined){
+          productMap[currentItem]= 0;
+        }
+         productMap[currentItem]= productMap[currentItem] + Number(NumberOfItem);
+
+     });
+
+            console.log(productMap);
+            callback(null, productMap);
+}
+
 };
